495a193b2532a5b26d64bec2ba4d84d82d9538d1
Christian Boxdörfer <christian.boxdoerfer@tu-ilmenau.de>
2013-05-15 09:12:46 +0200

    Formatierung der embedded.cpp überarbeitet. x86_64 version hinzugefügt.

--- embedded_pc/proto-emb/embedded.cpp
+++ embedded_pc/proto-emb/embedded.cpp
@@ -9,12 +9,15 @@ void sendData(string estr);
 
 int main()
 {
+	cout << "+++++++++++++++++++++++++++++++++++++++++++++++++++++" << endl;
 	cout << "Testprogramm zur Verdeutlichung der Programmstruktur." << endl;
-	cout << " Definiere Testsignal" << endl;
-	string testString = "Testsignal vom Embedded-PC.";
+	cout << "+++++++++++++++++++++++++++++++++++++++++++++++++++++" << endl;
+	cout << endl;
+	cout << "Definiere Testsignal..." << endl;
+	string testString = "Testsignal vom Embedded-PC. ";
 
 	
-	cout << " Rufe Funktion recvData auf.\n";
+	cout << "Rufe Funktion recvData auf..." << endl;
 	recvData(testString);
 
 
@@ -25,25 +28,25 @@ int main()
 
  void recvData(string estr)
 {
-	cout << "  recvData aufgerufen. Erhaltene Daten:\n";
-	cout << "   " + estr << endl;
+	cout << "Erhaltene Daten:" << endl << endl;
+	cout << "   " + estr << endl << endl;
 	
-	cout << " Rufe Funktion handleData auf.\n";
+	cout << "Rufe Funktion handleData auf..." << endl;
 	handleData(estr);
 }
 
 void handleData(string estr)
 {
-	cout << "  handleData aufgerufen. Erweiterung des Testsignals.\n";
-	estr = estr + " Von handleData hinzugefügter String.";
+	cout << "Erweiterung des Testsignals..." << endl;
+	estr = estr + "Von handleData hinzugefügter String.";
 
-	cout << " Rufe Funktion sendData auf.\n";
+	cout << "Rufe Funktion sendData auf..." << endl << endl;
 	sendData(estr);
 }
 
 void sendData(string estr)
 {
-	cout << "  sendData aufgerufen. Erhaltene Daten:\n";
-	cout << "   " + estr << endl;
+	cout << "Erhaltene Daten:" << endl;
+	cout << endl << "   " + estr << endl;
 }
 
