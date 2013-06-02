d48bc78df2d6d551ef02b6b1d2336183d73a06d3
unknown <C:\Users\Christian\AppData\Roaming\The Bat!>
2013-06-02 08:35:16 +0200

    fixed formatting

--- src/testing_00/embPCTime.cpp
+++ src/testing_00/embPCTime.cpp
@@ -21,115 +21,108 @@
 #include <sys/time.h> //fuer  gettimeofday
 using namespace std;
 
-char timestamp[6433];		//Zeitstempel String groesse 20 Zeichen
+char timestamp[6433];       //Zeitstempel String groesse 20 Zeichen
 char muell[6433];
 char buf[6416];
 
 
 
-void Empfangen(){
-	cout << "\n EMPFANGEN:" << endl;
-  int sockfd;
-  struct sockaddr_in my_addr, remote_addr;
-  socklen_t remote_addr_size = sizeof(remote_addr);
+void Empfangen()
+{
+    cout << "\n EMPFANGEN:" << endl;
+    int sockfd;
+    struct sockaddr_in my_addr, remote_addr;
+    socklen_t remote_addr_size = sizeof(remote_addr);
 
 
-  if ((sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0){
-     // fprintf(stderr, "Error: socket()\n");
-	  cout << "\n socketERROR:" << endl;
-     // return(1);
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0) {
+        // fprintf(stderr, "Error: socket()\n");
+        cout << "\n socketERROR:" << endl;
+        // return(1);
     }
 
-  my_addr.sin_family      = AF_INET;
-  my_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-  my_addr.sin_port        = htons(5000);
+    my_addr.sin_family      = AF_INET;
+    my_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    my_addr.sin_port        = htons(5000);
 
-  if (bind(sockfd, (struct sockaddr *)&my_addr, sizeof(my_addr)) < 0){
-      //fprintf(stderr, "Error: bind()\n");
-	  cout << "\n bindERROR:" << endl;
-      shutdown(sockfd,2);
-      //return(1);
+    if (bind(sockfd, (struct sockaddr *)&my_addr, sizeof(my_addr)) < 0) {
+        //fprintf(stderr, "Error: bind()\n");
+        cout << "\n bindERROR:" << endl;
+        shutdown(sockfd,2);
+        //return(1);
     }
 
-//for(;;){ später mit poll
-  if (recvfrom(sockfd, buf, sizeof(buf), 0, (struct sockaddr *)&remote_addr, &remote_addr_size) > 0){
-      //printf("Getting Data from %s\n", inet_ntoa(remote_addr.sin_addr.s_addr) );
+    //for(;;){ später mit poll
+    if (recvfrom(sockfd, buf, sizeof(buf), 0, (struct sockaddr *)&remote_addr, &remote_addr_size) > 0) {
+        //printf("Getting Data from %s\n", inet_ntoa(remote_addr.sin_addr.s_addr) );
+        printf("Daten: %s\n", buf);
+    }
 
-      printf("Daten: %s\n", buf);
-
-}
-
-	//printf("ENDE\n");
-  cout << "\n ENDE:" << endl;
-}
-//}
-
-
-
-void Zeitstempel(){
-	  cout << "\n TIME:" << endl;
-int sekund=0;			//dek. Sekunden
-int mikro=0;			//dekl. Mikrosekunden
-struct timeval zeit1;
- //sleep(1);
- gettimeofday(&zeit1,NULL);
-
- sekund = zeit1.tv_sec;
- mikro = zeit1.tv_usec;
-  sprintf(timestamp,"%d%d", sekund,mikro);
+    //printf("ENDE\n");
+    cout << "\n ENDE:" << endl;
 }
 
 
 
 
-void Senden(){
-	  cout << "\n SENDEN:" << endl;
-	//strcat(rack,muell);  //evtl fuer Zeitstempel
-	int sockfd;
-	  struct sockaddr_in remote_addr;
-	  struct hostent *host_addr;
+void Zeitstempel()
+{
+    cout << "\n TIME:" << endl;
+    int sekund=0;           //dek. Sekunden
+    int mikro=0;            //dekl. Mikrosekunden
+    struct timeval zeit1;
+    //sleep(1);
+    gettimeofday(&zeit1,NULL);
 
-
-	  char Host [33]="";
-	  //char Host [33]="s16929463.onlinehome-server.info";  //www.eulerbox.no-ip.biz
-	  char IP [20]= "87.106.17.165"; // 192.168.56.39
-
-
-	  if ((gethostbyname(Host)) == NULL){
-		  printf("HOSTERROR\n");
-
-		  memset(&remote_addr, 0, sizeof remote_addr);
-		  remote_addr.sin_addr.s_addr = inet_addr(IP);
-	  }
-
-	  else{
-		  host_addr = gethostbyname(Host);
-		  remote_addr.sin_addr   = *((struct in_addr *) host_addr->h_addr);
-	      }
+    sekund = zeit1.tv_sec;
+    mikro = zeit1.tv_usec;
+    sprintf(timestamp,"%d%d", sekund,mikro);
+}
 
 
 
-	  if ((sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0){
-	      fprintf(stderr, "Error: socket()\n");
-	      //return(1);
-	    }
 
-	  remote_addr.sin_family = AF_INET;
-	 // remote_addr.sin_addr   = *((struct in_addr *) host_addr->h_addr);
-	  remote_addr.sin_port   = htons(7777); //http://www.beej.us/guide/bgnet/output/html/multipage/htonsman.html
-
-
-	  if (sendto(sockfd, muell, strlen(muell) + 1, 0, (struct sockaddr *)&remote_addr, sizeof(remote_addr)) > 0){
-	      printf("Sendung erfolgreich!\n");
-	    }
-
-	  else {
-	      fprintf(stderr, "Fehler beim Senden!\n");
-	       }
+void Senden()
+{
+    cout << "\n SENDEN:" << endl;
+    //strcat(rack,muell);  //evtl fuer Zeitstempel
+    int sockfd;
+    struct sockaddr_in remote_addr;
+    struct hostent *host_addr;
 
 
-	  shutdown(sockfd,2);
+    char Host [33]="";
+    //char Host [33]="s16929463.onlinehome-server.info";  //www.eulerbox.no-ip.biz
+    char IP [20]= "87.106.17.165"; // 192.168.56.39
 
+
+    if ((gethostbyname(Host)) == NULL) {
+        printf("HOSTERROR\n");
+        memset(&remote_addr, 0, sizeof remote_addr);
+        remote_addr.sin_addr.s_addr = inet_addr(IP);
+    } else {
+        host_addr = gethostbyname(Host);
+        remote_addr.sin_addr   = *((struct in_addr *) host_addr->h_addr);
+    }
+
+
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0) {
+        fprintf(stderr, "Error: socket()\n");
+        //return(1);
+    }
+
+    remote_addr.sin_family = AF_INET;
+    // remote_addr.sin_addr   = *((struct in_addr *) host_addr->h_addr);
+    remote_addr.sin_port   = htons(7777); //http://www.beej.us/guide/bgnet/output/html/multipage/htonsman.html
+
+
+    if (sendto(sockfd, muell, strlen(muell) + 1, 0, (struct sockaddr *)&remote_addr, sizeof(remote_addr)) > 0) {
+        printf("Sendung erfolgreich!\n");
+    } else {
+        fprintf(stderr, "Fehler beim Senden!\n");
+    }
+
+    shutdown(sockfd,2);
 }
 
 
@@ -139,18 +132,17 @@ void Senden(){
 
 int main()
 {
-for(;;){
-Empfangen();
-printf("Daten: %s\n", buf);
-Zeitstempel();
-printf("\n Zeitstempel: %s\n",timestamp);
-strcat(timestamp,buf);
-strcat(muell,timestamp);
-Senden();
-muell[0]='\0';
-buf[0]='\0';
-}
-
+    for(;;) {
+        Empfangen();
+        printf("Daten: %s\n", buf);
+        Zeitstempel();
+        printf("\n Zeitstempel: %s\n",timestamp);
+        strcat(timestamp,buf);
+        strcat(muell,timestamp);
+        Senden();
+        muell[0]='\0';
+        buf[0]='\0';
+    }
 // schauen das gelichzeitig gesendet und empfangen werden kann?
 }
 
--- src/testing_00/umwandlungstest.cpp
+++ src/testing_00/umwandlungstest.cpp
@@ -19,7 +19,7 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <arpa/inet.h>
-#include<fstream>
+#include <fstream>
 #include <unistd.h>
 using namespace std;
 
@@ -28,81 +28,69 @@ short shZahlen[402];
 char Zeit[16];
 
 
-void Empfangen(){
-	cout << "\n EMPFANGEN:" << endl;
-  int sockfd;
-  struct sockaddr_in my_addr, remote_addr;
-  socklen_t remote_addr_size = sizeof(remote_addr);
+void Empfangen()
+{
+    cout << "\n EMPFANGEN:" << endl;
+    int sockfd;
+    struct sockaddr_in my_addr, remote_addr;
+    socklen_t remote_addr_size = sizeof(remote_addr);
 
 
-  if ((sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0){
-     // fprintf(stderr, "Error: socket()\n");
-	  cout << "\n socketERROR:" << endl;
-     // return(1);
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0) {
+         // fprintf(stderr, "Error: socket()\n");
+        cout << "\n socketERROR:" << endl;
+         // return(1);
     }
 
-  my_addr.sin_family      = AF_INET;
-  my_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-  my_addr.sin_port        = htons(7777);
+    my_addr.sin_family      = AF_INET;
+    my_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    my_addr.sin_port        = htons(7777);
 
-  if (bind(sockfd, (struct sockaddr *)&my_addr, sizeof(my_addr)) < 0){
-      //fprintf(stderr, "Error: bind()\n");
-	  cout << "\n bindERROR:" << endl;
-      shutdown(sockfd,2);
-      //return(1);
+    if (bind(sockfd, (struct sockaddr *)&my_addr, sizeof(my_addr)) < 0) {
+        //fprintf(stderr, "Error: bind()\n");
+        cout << "\n bindERROR:" << endl;
+        shutdown(sockfd,2);
+        //return(1);
     }
 
-//for(;;){ später mit poll
-  if (recvfrom(sockfd, buf, sizeof(buf), 0, (struct sockaddr *)&remote_addr, &remote_addr_size) > 0){
-      //printf("Getting Data from %s\n", inet_ntoa(remote_addr.sin_addr.s_addr) );
+    //for(;;){ später mit poll
+    if (recvfrom(sockfd, buf, sizeof(buf), 0, (struct sockaddr *)&remote_addr, &remote_addr_size) > 0) {
+        //printf("Getting Data from %s\n", inet_ntoa(remote_addr.sin_addr.s_addr) );
+        printf("Daten: %s\n", buf);
+    }
 
-      printf("Daten: %s\n", buf);
-
-}
-
-	//printf("ENDE\n");
-  cout << "\n ENDE:" << endl;
+    //printf("ENDE\n");
+    cout << "\n ENDE:" << endl;
 }
 //}
-void Zeitstempel(){
-	for (int k=0; k<16; k++){
-		Zeit[k]=buf[k];
-	}
+void Zeitstempel()
+{
+    for (int k=0; k<16; k++) {
+            Zeit[k]=buf[k];
+    }
 
 }
 
-
-void Umwandeln(){
-
-	for (int i=1; i<402; i++)
-	{
-	    shZahlen[i] = 0;
-	    for (int j=0; j<16; j++)
-	    {
-	        if (buf[i*16+j] != '0')
-	            shZahlen[i] += 1 << (15-j);
-	    }
-	    cout << shZahlen[i] << endl;
-	}
+void Umwandeln()
+{
+    for (int i=1; i<402; i++) {
+        shZahlen[i] = 0;
+        for (int j=0; j<16; j++) {
+            if (buf[i*16+j] != '0')
+                shZahlen[i] += 1 << (15-j);
+        }
+        cout << shZahlen[i] << endl;
+    }
 
 }
 
-
-
-
-
-
-
-
-
-
-
-int main() {
-	Empfangen();
-	Zeitstempel();
-	Umwandeln();
-	printf("Zeitstempel");
-	cout << Zeit << endl;
+int main() 
+{
+    Empfangen();
+    Zeitstempel();
+    Umwandeln();
+    printf("Zeitstempel");
+    cout << Zeit << endl;
 
 }
 
@@ -118,52 +106,47 @@ double d;
 
 int main() {
 
-	for(int j=sizeof(test);j<34;j++){
+    for(int j=sizeof(test);j<34;j++){
 
 
-	for(int i=sizeof(help);i<18;i++){
+    for(int i=sizeof(help);i<18;i++){
 
-	for(int a=0;a<=15;a++){
-		if(test[a]=='1'){
-		strcat(help,"1");
-		}
-		else{
-			strcat(help,"0");
-		}
+    for(int a=0;a<=15;a++){
+        if(test[a]=='1'){
+            strcat(help,"1");
+        }else{
+            strcat(help,"0");
+        }
 }
-	printf("\nhelp: %s\n",help);
-d = atof(help);
+    printf("\nhelp: %s\n",help);
+    d = atof(help);
 
-//printf("\n Double: %f\n",d);
-int x = (int) d;
-	cout << x << endl; // prints
-d=0;
-	}
-	help[0]='\0';
-	}
-
-
-
-	//test=1111111111000011;
-	int x = (int) 0b1111111111000011;
-	cout << x << endl; // prints
-
-	for(int u=0;u<17;u++){
-	for(j=(sizeof(test[u]) * 8) - 1; j >= 0;j--){
-
-	 if(test[u] & (1 << j)){
-		printf("1\n");
-
-	 }
-	  else      {
-		printf("0\n");
-	  } //if ENDE
-	} 	//for ENDE
-	}
+    //printf("\n Double: %f\n",d);
+    int x = (int) d;
+    cout << x << endl; // prints
+    d=0;
+    }
+    help[0]='\0';
+    }
 
 
-	return 0;
 
+    //test=1111111111000011;
+    int x = (int) 0b1111111111000011;
+    cout << x << endl; // prints
+
+    for(int u=0;u<17;u++){
+        for(j=(sizeof(test[u]) * 8) - 1; j >= 0;j--){
+
+            if(test[u] & (1 << j)){
+                printf("1\n");
+            }
+            else {
+                printf("0\n");
+            } //if ENDE
+        }   //for ENDE
+    }
+    return 0;
 }
 */
 
