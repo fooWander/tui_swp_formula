38c1459696f709ee6bb8e73e7301787d7daac790
Christian Boxdoerfer <christian.boxdoerfer@tu-ilmenau.de>
2013-06-12 16:21:32 +0200

    fixed coding style

--- src/testing_01/test.cpp
+++ src/testing_01/test.cpp
@@ -25,8 +25,7 @@ void createHeader()
 int getPackage(char * package, unsigned short packageNumber)
 {
     int len;
-    if (packageNumber == 1)
-    {
+    if (packageNumber == 1) {
         len = myPackagesPos[packageNumber] - 1;
         //std::cout << myPackagesPos[packageNumber] - 1 << std::endl;
     } else {
@@ -34,8 +33,7 @@ int getPackage(char * package, unsigned short packageNumber)
         //std::cout << myPackagesPos[packageNumber] << std::endl;
         //std::cout << myPackagesPos[packageNumber - 1] << std::endl;
     }
-    for (int i = 0; i < len; ++i)
-    {
+    for (int i = 0; i < len; ++i) {
         package[i] = myPackages[myPackagesPos[packageNumber - 1] + i];
         //std::cout << myPackagesPos[packageNumber - 1 + i] << std::endl;
     }
@@ -55,8 +53,7 @@ void splitData(const char *buffer, const int bufferlen, const char *vecLayout, c
             myPackagesPointer++;
         }
 
-        if (pointer == 0)
-        {
+        if (pointer == 0) {
             myPackagesPos[pointer] = splitPos + 4;
         } else if (pointer != myPackagesSum) {
             myPackagesPos[pointer] = myPackagesPos[pointer-1] + splitPos + 4;
@@ -79,13 +76,11 @@ int main(int argc, char const *argv[])
     //createHeader();
     splitData(buffer,bufferlen,vecLayout,vecLayoutlen);
 
-    for (int i = 0; i < myPackagesPointer; ++i)
-    {
+    for (int i = 0; i < myPackagesPointer; ++i) {
         std::cout << "Wert " << i << ":" << myPackages[i] << std::endl;
     }
 
-    for (int i = 0; i < myPackagesSum; ++i)
-    {
+    for (int i = 0; i < myPackagesSum; ++i) {
         std::cout << "Paketposition: " << myPackagesPos[i] << std::endl;
     }
 
@@ -94,8 +89,7 @@ int main(int argc, char const *argv[])
     int len = getPackage(package,2);
     package[len] = '\0';
 
-    for (int i = 0; i < len; ++i)
-    {
+    for (int i = 0; i < len; ++i) {
         //std::cout << sizeof(package) << std::endl;
         std::cout << package[i] << std::endl;
     }
