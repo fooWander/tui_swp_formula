63a25b7f2e2e2210bd78ae58cda09df3f60727db
TT-SWP13 <tino.tausch@tu-ilmenau.de>
2013-06-15 18:10:20 +0200

    Latenz erweitert

--- src/realtime.cpp
+++ src/realtime.cpp
@@ -9,20 +9,19 @@
  *			--> ständige Abfragen vermeiden, da Abfrage etwas zeitintensiv
  *
  */
+#include <sys/types.h>
 #include <stdio.h>
 #include <string.h>
-#include <unistd.h>
 #include <time.h>
 #include <string.h>
 #include <iostream>
-#include <sys/types.h>
-
+#include <unistd.h>
 using namespace std;
 
 #define NSEC_PER_SEC 1000000000ULL //unisigned long long - fuer Überlauf/Normierung
-static struct timespec before, after;
+static struct timespec old, latest;
 
-int64_t timespec_to_ns(struct timespec *t) {
+int64_t TO_NANO_SEC(struct timespec *t) {
   int64_t result;
 
   result	=  (int64_t)t->tv_sec;
@@ -34,30 +33,33 @@ int64_t timespec_to_ns(struct timespec *t) {
 int main(/*int argc, char *argv[]*/)
 {
 	cout<<"Bitte warten..."<<endl;
-	int64_t		timedifference;
+    cout<<" "<<endl;
+	int64_t	difference;
+	int64_t	latency;
 
-	clock_gettime(CLOCK_REALTIME, &before);
+	clock_gettime(CLOCK_REALTIME, &old);
+	//Aenderungen an sleep auch bei latency durchfuehren
 	sleep(5);
 	usleep(100000);
-    clock_gettime(CLOCK_REALTIME, &after);
+    clock_gettime(CLOCK_REALTIME, &latest);
 
-    cout<<"Fertig!"<<endl;
-    cout<<" "<<endl;
-
-    timedifference = timespec_to_ns(&after) - timespec_to_ns(&before);
+    difference = TO_NANO_SEC(&latest) - TO_NANO_SEC(&old);
 
     	//Standardausgabe:
-    	cout<<"Vorher: "<<timespec_to_ns(&before)<<endl;
-    	cout<<"Danach: "<<timespec_to_ns(&after)<<endl;
+    	cout<<"Vorher: "<<TO_NANO_SEC(&old)<<endl;
+    	cout<<"Danach: "<<TO_NANO_SEC(&latest)<<endl;
     	cout<<" "<<endl;
-    	cout<<"Differenz: "<<timedifference<<endl;
+    	cout<<"Differenz: "<<difference<<endl;
 
     	fprintf(stderr,"\n\n");
 
     	//Formatiert:
-    	fprintf(stderr,"this is before sleep: %lli.%09lli\n", ((long long)timespec_to_ns(&before))/NSEC_PER_SEC, ((long long)timespec_to_ns(&before))%NSEC_PER_SEC);
-    	fprintf(stderr,"this is after sleep: %lli.%09lli\n", ((long long)timespec_to_ns(&after))/NSEC_PER_SEC, ((long long)timespec_to_ns(&after))%NSEC_PER_SEC);
+    	fprintf(stderr,"this is before sleep: %lli.%09lli\n",((long long)TO_NANO_SEC(&old))/NSEC_PER_SEC, ((long long)TO_NANO_SEC(&old))%NSEC_PER_SEC);
+    	fprintf(stderr,"this is after sleep: %lli.%09lli\n",((long long)TO_NANO_SEC(&latest))/NSEC_PER_SEC, ((long long)TO_NANO_SEC(&latest))%NSEC_PER_SEC);
     	fprintf(stderr,"\n\n");
-    	fprintf(stderr,"Timediff: %lli.%09lli\n", ((long long)timedifference)/NSEC_PER_SEC, ((long long)timedifference)%NSEC_PER_SEC);
+    	fprintf(stderr,"difference: %lli.%09lli\n", ((long long)difference)/NSEC_PER_SEC, ((long long)difference)%NSEC_PER_SEC);
     	fprintf(stderr,"\n");
+
+    	latency = difference - 5100000000; //= rund 225447 (auf Laptop)
+    	cout<<"Latenz: "<<latency<<endl;
 }
