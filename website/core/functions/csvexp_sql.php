590c789d43d9470e688b0a2f8f073be8f603c2c5
Thomas <thomas.golda@tu-ilmenau.de>
2013-06-15 15:51:12 +0200

    CSV Funzt. Kein ERROR 500 mehr. :3

--- website/core/functions/csvexp_sql.php
+++ website/core/functions/csvexp_sql.php
@@ -1,87 +1,73 @@
 <?php 
 
-session_start();
-error_reporting(E_ALL);
-
 include '../../includes/config.php';
 
-/**
- *
- * 	Baue Verbindung zur Datenbank auf und wähle die richtige Datenbank aus.
- *  Danach führe Datenbankoperation aus und speichere Ergebnis in JSON-Format
- *
- */
+header("Content-type: text/csv");
+header("Content-Disposition: attachment; filename=file.csv");
+header("Pragma: no-cache");
+header("Expires: 0");
 
 if (empty($_POST) == false) {
-	$connect = mysqli_connect("$dbhost", "$dbuname" , "$dbpass") or die("<tr><td colspan=\"3\">Verbindung zur Datenbank konnte nicht hergestellt werden.</td></tr>");
-	$use = mysqli_select_db($connect,"$dbname_fd");
-	
-	//print_r $_POST;
 
+	$connect = mysqli_connect("$dbhost", "$dbuname", "$dbpass") or die("Verbindung zur Datenbank konnte nicht hergestellt werden.\n<br>\n");
+	mysqli_select_db($connect,"$dbname_fd");
+	
+	
 	if(array_key_exists('alledaten',$_POST))
 	{
-		$sql = "SELECT  *
-		FROM `$general_data`
-		NATURAL JOIN `$accu_data`
-		NATURAL JOIN `$dynamic_data`
-		NATURAL JOIN `$driving_data`
-		NATURAL JOIN `$engine_data`";
+		$sql = "SELECT  * FROM `" . $general_data . "` NATURAL JOIN `" . $accu_data	. "` NATURAL JOIN `" . $dynamic_data . "` NATURAL JOIN `" . $driving_data . "` NATURAL JOIN `" . $engine_data . "`";
 	}
 	else
 	{
 		$val=array();
-		if(array_key_exists('choice1',$_POST)){$val=array_merge($val,[$general_data]);};
-		if(array_key_exists('choice2',$_POST)){$val=array_merge($val,[$accu_data]);};
-		if(array_key_exists('choice3',$_POST)){$val=array_merge($val,[$dynamic_data]);};
-		if(array_key_exists('choice4',$_POST)){$val=array_merge($val,[$driving_data]);};
-		if(array_key_exists('choice5',$_POST)){$val=array_merge($val,[$engine_data]);};
-		$sql = "SELECT  *  FROM	`" . implode("` NATURAL JOIN `",$val) . "`";
-		echo $sql;
+		if(array_key_exists('choice1',$_POST)){$val[] = $general_data;}
+		if(array_key_exists('choice2',$_POST)){$val[] = $accu_data;}
+		if(array_key_exists('choice3',$_POST)){$val[] = $dynamic_data;}
+		if(array_key_exists('choice4',$_POST)){$val[] = $driving_data;}
+		if(array_key_exists('choice5',$_POST)){$val[] = $engine_data;}
+		$sql = "SELECT  *  FROM	`" . implode("` NATURAL JOIN `",$val);
 	}
-		
-	$query= mysqli_query($connect,$sql);			// Führe die DB Operation aus
+	
+	// Führe die DB Operation aus
+	$query = mysqli_query($connect,$sql);			
 
-	if (!$query) 								// Fehlerbehandlung
+	// Fehlerbehandlung
+	if (!$query) 								
 	{
 		echo 'Konnte Abfrage nicht ausführen: ' . mysqli_error($connect);
 		exit;
 	}
 	mysqli_close($connect);
 
-
-	$csvString = "";
-	$csvcolumns= "";
-	$count=0;
-	while($ret=$query->fetch_row())
-	{
-		for($k=0;$k<count($ret);$k++)
-		{
-			$query->field_seek($k);
-			$finfo = $query->fetch_field();		// Tabellenüberschriften sammeln
-			if($k==0)
-			{
-				$csvString.="$ret[$k]";		// beschreibe die erst Stelle, sonst leer (alles um eins nach rechts verschoben)
+	// Hilfsboolean, damit erste Schleife für Kopf nur einmal ausgeführt wird
+	$bool = true;
+	
+	// Für jede Zeile der Tabelle die einzelnen Felder ausgeben
+	while($unter = mysqli_fetch_array($query, MYSQLI_ASSOC)) {
+		if ($bool == true) {
+			$tmp = array_keys($unter);
+			
+			for($i = 0; $i < count($tmp); $i++) {		
+				if ($i == count($unter) - 1) {
+					echo $tmp[$i] . "\n";
+				} else {
+					echo $tmp[$i] . ", ";
+				}
 			}
-			else
-			{
-				$csvString.=";\"".$ret[$k]."\"";			// bearbeite Werte so, dass sie CSV konform sind
-			}
-			if(!$count) $csvcolumns.=($csvcolumns?";":"").$finfo->name;		// trenne Daten richtig
+			
+			$bool = false;
 		}
-		$csvString.="\n";
-		$count++;
+		$unter = array_values($unter);
+		$bla = count($unter);
+		for($i = 0; $i < count($unter); $i++) {
+			
+			if ($i == count($unter) - 1) {
+				echo $unter[$i] . "\n";
+			} else {
+				echo $unter[$i] . ", ";
+			}
+			
+		}
 	}
-	$csvdata= str_replace(["\n","."],["\n",","],$csvcolumns."\n".$csvString);	// ersetze "." durch "," Floatwerte sonst als Datum angezeigt
-	
-	$outstream = fopen("php://output",'w');
-
-	header('Content-type: text/csv');
-	header('Cache-Control: no-store, no-cache');
-	header('Content-Disposition: attachment; filename=tsc02e.csv');	
-	
-	echo $csvdata;
-	 
-	fclose($outstream);
-	
 }
 ?>
