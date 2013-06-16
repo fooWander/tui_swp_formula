c585f803f839b58aa453706787933117d587d1ee
Tobias Zehner <tobiaszehner1@gmail.com>
2013-06-16 23:40:27 +0200

    Doxygen-Kommentare
    Noch mehr Kommentare :D

--- src/tobias_klassen/DatatypeDaemon.h
+++ src/tobias_klassen/DatatypeDaemon.h
@@ -6,19 +6,56 @@
 #include <boost/lexical_cast.hpp>
 #include "Data.h"
 #include "Decoder.h"
-
 using namespace std;
 using namespace boost;
 
-class DatatypeDaemon{
-	public:
-		DatatypeDaemon();
-		string parseNextValue();
-		int getPosActualPacket();
-		int getTime();
-	private:
-		unsigned int dataType;
-		int pos;
-};
+	/*! \brief Klasse zum Aufbereiten der Daten.
+	*
+	*	In dieser Klasse werden Die Daten nicht nur in das richtige Format umgewandelt,
+	*	sondern auch durch eine Zehnerpotenz geteilt, sodass sie wieder ihrem Wert vor
+	*	der Übertragung entsprechen.
+	*	Es werden weiterhin Zusatzinformationen angeboten, die es sowohl ermöglichen, die
+	*	Paketnummer und damit die Tabelle, in die eingefügt werden soll, zu ermitteln,
+	*	als auch den Zeitstempel der Daten einzufügen.
+	*	Diese Klasse dient also als Schnittstelle für DBPacketInsert zum Decoder.
+	*	
+	*	\sa Data
+	*/
+	class DatatypeDaemon{
+		public:
+			/*! Konstruktor der Klasse.
+			*
+			*/
+			DatatypeDaemon();
+			/*! Diese Funktion wird von DBPacketInsert aufgerufen.
+			*
+			*	Sie holt sich ein Data Objekt vom Decoder und extrahiert daraus
+			*	die Nutzdaten und die Position dieser Daten im Paket. 
+			*	Die Position wird dabei in einer Membervariablen zwischengespeichert.
+			*	Nachdem der Datentyp bestimmt wurde, wird eine Divison durch eine Zehnerpotenz
+			*	entsprechend dem jetzt erst extrahierten Wert aus Data durchgeführt, um den Wert
+			*	auf seine ursprüngliche Größe zurückzubringen. Dies wird jedoch nur ausgeführt,
+			*	wenn der Wert vor der Übertragung ein Gleitkommawert war.
+			*	Abschließend wird der Wert in einen String umgewandelt, der dann an den Aufrufer 
+			*	zurückgegeben wird.
+			*/
+			string parseNextValue();
+			/*! Diese Funktion liefert die Position des jetzigen Datenwerts im Übertragungsvektor
+				der MikroAutoBoxII zurück.
+			*
+			*	Dies ist für die Bestimmung des Übertragungspakets vom Embedded PC wichtig, da
+			*	darauf basierend die richtige Datenbankanfrage in DBPacketInsert aufgebaut wird.
+			*
+			*	\return Aus Data extrahierter und aufbereiteter Wert im String-Format.
+			*/
+			int getPosActualPacket();
+			/*! Diese Memberfunktion gibt den Zeitstempel des aktuellen Pakets zurück.
+			* \return Zeitstempel des Pakets.
+			*/
+			int getTime();
+		private:
+			unsigned int dataType;	//!< Datentyp des aktuellen Werts.
+			int pos;				//!< Position des Werts im Übertragungsvektor der MikroAutoBoxII.
+	};
 
 #endif
--- src/tobias_klassen/dbData.h
+++ src/tobias_klassen/dbData.h
@@ -6,19 +6,47 @@
 #include <string>
 using namespace std;
 
+	/**	\brief Klasse zum Auslesen der Zugangsdaten für die Datenbank.
+	*
+	*	Diese Klasse liest die Zugangsdaten aus einer Textdatei (dbconfig.txt) aus und stellt
+	*	sie über Memberfunktionen bereit.
+	*	\sa Insert
+	*
+	*/
   class dbData
   {
     public:
+	  /**
+	  *	Konstruktor der Klasse. Hier werden die Zugangsdaten aus der Textdatei in die entsprechenden
+	  * lokalen Variablen eingelesen.
+	  */
       dbData();
+	  //! Memberfunktion, die die Hostadresse bereitstellt.
+	  /*!
+	  *	Wird von Insert aufgerufen.
+	  * \return Die Hostadresse.
+	  */
       string getHost();
+	  //! Memberfunktion, die den Benutzernamen für die Datenbank bereitstellt.
+	  /*!
+	  *	\return Der Nutzername.
+	  */
       string getUser();
+	  //! Memberfunktion, die das Passwort der Datenbank bereitstellt.
+	  /*!
+	  *	\return Das Passwort für die Datenbank und den Nutzer.
+	  */
       string getPW();
+	  //! Memberfunktion, die den Namen der Datenbank bereitstellt.
+	  /*!
+	  *	\return Der Name der Datenbank.
+	  */
       string getDB();
     private:
-      string host;
-      string user;
-      string pw;
-      string db;
+      string host;	//!< speichert die Hostadresse zwischen.
+      string user;	//!< speichert den Benutzernamen zwischen.
+      string pw;	//!< speichert das Passwort zwischen.
+      string db;	//!< speichert den Namen der Datenbank zwischen.
 
   };
 
