39e2537adc642bc1b52387d2726747bd1472b3a8
Thomas <thomas.golda@tu-ilmenau.de>
2013-06-23 11:42:50 +0200

    Password recovery
    Sollte auch drin sein. Mein GMX-Konto macht aber probleme. Mit dem Tu
    Ilmenau kram gehts.

--- website/mail.php
+++ website/mail.php
@@ -1,24 +1,30 @@
 <?php
 require 'includes/phpmailer/class.phpmailer.php';
 
-$mail = new PHPMailer;
+$mail = 'thomas.golda@tu-ilmenau.de';
 
-$mail->IsSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 's16929463.onlinehome-server.info';  // Specify main and backup server
-$mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'Telemetrie@s16929463.onlinehome-server.info';                            // SMTP username
-$mail->Password = 'e]jm&fNJz9';                           // SMTP password
-$mail->SMTPSecure = 'tls';                            // Enable encryption, 'ssl' also accepted
+$email = new PHPMailer;
 
-$mail->From = 'noreply@teamstarcraft.de';
-$mail->FromName = 'Team Starcraft e. V.';
-$mail->AddAddress('thomas.golda@tu-ilmenau.de');  // Add a recipient
-$mail->AddCC('thomas.golda@tu-ilmenau.de');
+$email->IsSMTP();                                      	// Set mailer to use SMTP
+$email->Host = 's16929463.onlinehome-server.info'; 		// Specify main and backup server
+$email->SMTPAuth = true;                               	// Enable SMTP authentication
+$email->Username = 'Telemetrie@s16929463.onlinehome-server.info';                            // SMTP username
+$email->Password = 'e]jm&fNJz9';                        // SMTP password
+$email->SMTPSecure = 'tls';                            	// Enable encryption, 'ssl' also accepted
 
-$mail->WordWrap = 50;                                 // Set word wrap to 50 characters
-$mail->IsHTML(true);                                  // Set email format to HTML
+$email->From = 'noreply@teamstarcraft.de';
+$email->FromName = 'Team Starcraft e. V.';
+$email->AddAddress($mail);  							// Add a recipient
+//$email->AddBCC('thomas.golda@tu-ilmenau.de');
 
-$mail->Subject = 'Neue Registrierung im Service Interface';
-$mail->Body    = 'Sehr geehrter Vorstand,<br><br>ein neuer Benutzer hat sich im System registriert. Bitte loggen Sie sich im Service Interface ein und überprüfen Sie seine Berechtigungen. Dort können Sie ihn auch freischalten.<br><br>Besten Dank!<br><br>(Diese E-Mail wurde automatisch generiert.)';
-$mail->AltBody = 'Sehr geehrter Vorstand, ein neuer Benutzer hat sich im System registriert. Bitte loggen Sie sich im Service Interface ein und überprüfen Sie seine Berechtigungen. Dort können Sie ihn auch freischalten. Besten Dank! (Diese E-Mail wurde automatisch generiert.)';
+$email->WordWrap = 50;                                 	// Set word wrap to 50 characters
+$email->IsHTML(true);                                  	// Set email format to HTML
+
+$email->Subject = 'Neue Registrierung im Service Interface';
+$email->Body    = "Sehr geehrter Vorstand,<br><br>ein neuer Benutzer hat sich im System registriert. Bitte loggen Sie sich im Service Interface ein und überprüfen Sie seine Berechtigungen. Dort können Sie ihn auch freischalten.<br><br>Besten Dank!<br><br>(Diese E-Mail wurde automatisch generiert.)";
+$email->AltBody = "Sehr geehrter Vorstand,\n\nein neuer Benutzer hat sich im System registriert. Bitte loggen Sie sich im Service Interface ein und überprüfen Sie seine Berechtigungen. Dort können Sie ihn auch freischalten.\n\nBesten Dank! (Diese E-Mail wurde automatisch generiert.)";
+
+if(!$email->Send()) {
+	echo "Mailer Error: " . $email->ErrorInfo;
+}
 ?>
