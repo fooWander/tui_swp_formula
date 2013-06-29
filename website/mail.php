17820145be5f7bc3f00e4ae9caf90f967607d7e1
Thomas <thomas.golda@tu-ilmenau.de>
2013-06-29 12:36:14 +0200

    Email-Anpassungen
    Email-Text bei Registrierung angepasst. Feinheiten ausgebessert. Work in
    Progress.

--- website/mail.php
+++ website/mail.php
@@ -1,28 +1,28 @@
 <?php
 require 'includes/phpmailer/class.phpmailer.php';
 
-//$mail = 'thomas.golda@tu-ilmenau.de';
-
 $email = new PHPMailer;
 
 $email->IsSMTP();                                      	// Set mailer to use SMTP
 $email->Host = 's16929463.onlinehome-server.info'; 		// Specify main and backup server
+//$email->Host = 'mail.gmx.net'; 						// Specify main and backup server
 $email->SMTPAuth = true;                               	// Enable SMTP authentication
 $email->Username = 'Telemetrie@s16929463.onlinehome-server.info';                            // SMTP username
 $email->Password = 'e]jm&fNJz9';                        // SMTP password
+//$email->Username = 'golda@gmx.net';                            // SMTP username
+//$email->Password = 'win91ter';                        // SMTP password
 $email->SMTPSecure = 'tls';                            	// Enable encryption, 'ssl' also accepted
 
 $email->From = 'noreply@teamstarcraft.de';
 $email->FromName = 'Team Starcraft e. V.';
 $email->AddAddress($mail);  							// Add a recipient
-//$email->AddBCC('thomas.golda@tu-ilmenau.de');
 
 $email->WordWrap = 50;                                 	// Set word wrap to 50 characters
 $email->IsHTML(true);                                  	// Set email format to HTML
 
 $email->Subject = 'Neue Registrierung im Service Interface';
-$email->Body    = "Sehr geehrter Vorstand,<br><br>ein neuer Benutzer <b>(" . $register_data['email'] . ")</b> hat sich im System registriert. Bitte loggen Sie sich im Service Interface ein und überprüfen Sie seine Berechtigungen. Dort können Sie ihn auch freischalten.<br><br>Besten Dank!<br><br>(Diese E-Mail wurde automatisch generiert.)";
-$email->AltBody = "Sehr geehrter Vorstand,\n\nein neuer Benutzer hat sich im System registriert. Bitte loggen Sie sich im Service Interface ein und überprüfen Sie seine Berechtigungen. Dort können Sie ihn auch freischalten.\n\nBesten Dank! (Diese E-Mail wurde automatisch generiert.)";
+$email->Body    = "Sehr geehrter Team Starcraft Vorstand,<br><br>ein neuer Benutzer <b>(" . $register_data['email'] . ")</b> hat sich im System registriert. Bitte loggen Sie sich im Service Interface ein und überprüfen Sie ob es sich um eine berechtigte Registrierung handelt. Dort können Sie ihn auch freischalten. Bitte kontaktieren Sie den Benutzer anschließend per Mail und weisen ihn ggf. auf die Freischaltung seines Accounts hin.<br><br>Besten Dank!<br><br>(Diese E-Mail wurde automatisch generiert.)";
+$email->AltBody = "Sehr geehrter Vorstand,\n\nein neuer Benutzer (" . $register_data['email'] . ") hat sich im System registriert. Bitte loggen Sie sich im Service Interface ein und überprüfen Sie ob es sich um eine berechtigte Registrierung handelt. Dort können Sie ihn auch freischalten. Bitte kontaktieren Sie den Benutzer anschließend per Mail und weisen ihn ggf. auf die Freischaltung seines Accounts hin.\n\nBesten Dank!\n\n(Diese E-Mail wurde automatisch generiert.)";
 
 if(!$email->Send()) {
 	echo "Mailer Error: " . $email->ErrorInfo;
